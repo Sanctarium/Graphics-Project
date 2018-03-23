9fd56daa3ce8bed19f1425b17ad8526f901e3a11
Игорь <ikozurov@gmail.com>
2018-03-23 16:35:51 +0500

    Move algorithms in another class

---
+++ WindowsFormsApp1/Algorithms.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Algorithms
+    {
+        static private int toInteger(double num)
+        {
+            if (Math.Abs(num - (int)num) < 0.5) return (int)num;
+            else
+                return (int)num + 1;
+        }
+        static private int getSign(double num)
+        {
+            if (num > 0) return 1;
+            if (num == 0) return 0;
+            else return -1;
+        }
+        static public void DDA(int x1, int y1, int x2, int y2, string colorname, Bitmap bitmap)
+        {
+            double length;
+            double x, y;
+            if (Math.Abs(x2 - x1) >= Math.Abs(y2 - y1))
+                length = Math.Abs(x2 - x1);
+            else
+                length = Math.Abs(y2 - y1);
+            double dx = (x2 - x1) / length, dy = (y2 - y1) / length * 1.0;
+            x = x1 + 0.5 * getSign(dx);
+            y = y1 + 0.5 * getSign(dy);
+            for (int i = 0; i < length; i++)
+            {
+                bitmap.SetPixel(toInteger(x), toInteger(y), Color.FromName(colorname));
+                x += dx;
+                y += dy;
+            }
+        }
+        static public void BrezenheimLine(int x1, int y1, int x2, int y2, string colorname, Bitmap bitmap)
+        {
+            int x = x1, y = y1;
+            int dx = Math.Abs(x2 - x1), dy = Math.Abs(y2 - y1),
+                s1 = getSign(x2 - x1), s2 = getSign(y2 - y1);
+            int temp, trade, e = 2 * dy - dx;
+            if (dy > dx)
+            {
+                temp = dx;
+                dx = dy;
+                dy = temp;
+                trade = 1;
+            }
+            else
+                trade = 0;
+            for (int i = 1; i < dx; i++)
+            {
+                bitmap.SetPixel(x, y, Color.FromName(colorname));
+                while (e >= 0)
+                {
+                    if (trade == 1)
+                        x = x + s1;
+                    else
+                        y = y + s2;
+                    e -= 2 * dx;
+                }
+                if (trade == 1)
+                    y += s2;
+                else
+                    x += s1;
+                e += 2 * dy;
+            }
+        }
+    }
+}
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -17,18 +17,6 @@ namespace WindowsFormsApp1
             Manager.PaintMode = Manager.PaintType.DDA;
         }
 
-        private int toInteger(double num)
-        {
-            if (Math.Abs(num - (int)num) < 0.5) return (int)num;
-            else
-                return (int)num + 1;
-        }
-        private int getSign(double num)
-        {
-            if (num > 0) return 1;
-            if (num == 0) return 0;
-            else return -1;
-        }
         private int GetPicX(int x)
         {
             if (x >= pictureBox1.ClientRectangle.Right)
@@ -46,68 +34,15 @@ namespace WindowsFormsApp1
             else return y;
         }
 
-        private void DDA(int x1, int y1, int x2, int y2, string colorname, Bitmap bitmap)
-        {
-            double length;
-            double x, y;
-            if (Math.Abs(x2 - x1) >= Math.Abs(y2 - y1))
-                length = Math.Abs(x2 - x1);
-            else
-                length = Math.Abs(y2 - y1);
-            double dx = (x2 - x1) / length, dy = (y2 - y1) / length*1.0;
-            x = x1 + 0.5 * getSign(dx);
-            y = y1 + 0.5 * getSign(dy);
-            for (int i = 0; i < length; i++)
-            {
-                bitmap.SetPixel(toInteger(x), toInteger(y), Color.FromName(colorname));
-                x += dx;
-                y += dy;
-            }
-            pictureBox1.Refresh();
-        }
-        private void BrezenheimLine(int x1, int y1, int x2, int y2, string colorname, Bitmap bitmap)
-        {
-            int x=x1, y=y1;
-            int dx = Math.Abs(x2 - x1), dy = Math.Abs(y2 - y1),
-                s1=getSign(x2-x1), s2=getSign(y2-y1);
-            int temp, trade, e=2*dy-dx;
-            if (dy > dx)
-            {
-                temp = dx;
-                dx = dy;
-                dy = temp;
-                trade = 1;
-            }
-            else
-                trade = 0;
-            for(int i=1; i<dx; i++)
-            {
-                bitmap.SetPixel(x, y, Color.FromName(colorname));
-                while(e>=0)
-                {
-                    if (trade == 1)
-                        x = x + s1;
-                    else
-                        y = y + s2;
-                    e -= 2 * dx;
-                }
-                if (trade == 1)
-                    y += s2;
-                else
-                    x += s1;
-                e += 2 * dy;
-            }
-            pictureBox1.Refresh();
-        }
         private void Plot(int x1, int y1, int x2, int y2, string color)
         {
             switch(Manager.PaintMode)
             {
                 case Manager.PaintType.DDA:
-                    DDA(x1, y1, x2, y2, color, Manager.Bitmap);
+                    Algorithms.DDA(x1, y1, x2, y2, color, Manager.Bitmap);
                     break;
                 case Manager.PaintType.BrezenheimLine:
-                    BrezenheimLine(x1, y1, x2, y2, color, Manager.Bitmap);
+                    Algorithms.BrezenheimLine(x1, y1, x2, y2, color, Manager.Bitmap);
                     break;
                 case Manager.PaintType.BrezenheimCircle:
                     //call BrezenheimCircle method
@@ -121,6 +56,7 @@ namespace WindowsFormsApp1
         {
             int x1 = GetPicX(Manager.GlobalX), y1 = GetPicY(Manager.GlobalY), x2 = GetPicX(e.X), y2 = GetPicY(e.Y);
             Plot(x1, y1, x2, y2, "black");
+            pictureBox1.Refresh();
             Manager.DrawAccessible = true;
         }
 
